25e4512f34147b283e23e6741e914b4e341fa7b7
alexarirok <akorir233@gmail.com>
2019-07-03 14:11:50 +0300

    Create render_templates and word processing

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 import os
-from flask import Flask  
+import requests
+from flask import Flask, render_template, request 
 from flask_sqlalchemy import SQLAlchemy 
 
 
 app = Flask(__name__)
-#app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
 from models import Result
 
-@app.route('/')
-def home():
-    return "Welcome Home!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = []
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure its valid and try again"
+            )
 
-@app.route('/<about>')
-def about_home(about):
-    return "Welcome {}!".format(about)
+    return render_template('index.html', errors=errors,results=results)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
